e6f62ff08a68d320e47277bee5e4956efd4a48d2
niedexi <niedexi10@gmail.com>
2018-10-31 13:57:42 -0400

    .

--- Python/14_Longest_Common_Prefix.py
+++ Python/14_Longest_Common_Prefix.py
@@ -4,15 +4,6 @@ strings.
 
 If there is no common prefix, return an empty string "".
 
-Example 1:
-
-Input: ["flower","flow","flight"]
-Output: "fl"
-Example 2:
-
-Input: ["dog","racecar","car"]
-Output: ""
-Explanation: There is no common prefix among the input strings.
 Note:
 
 All given inputs are in lowercase letters a-z.
@@ -24,4 +15,8 @@ class Solution:
         :type strs: List[str]
         :rtype: str
         """
+
+        if not strs:
+            return ""
+
         
