aeb0286c467632448d851cb4a9a0096b723d8bad
niedexi <niedexi10@gmail.com>
2018-11-24 16:30:35 -0500

    add 14

--- Java/14LongestCommonPrefix.java
+++ Java/14LongestCommonPrefix.java
@@ -1,14 +1,10 @@
 /*
 Write a function to find the longest common prefix string amongst an array of 
 strings.
-
 If there is no common prefix, return an empty string "".
-
 Note:
-
 All given inputs are in lowercase letters a-z.
 */
-
 
 // Horizontal scanning
 public String longestCommonPrefix(String[] strs) {
--- Python/14LongestCommonPrefix.py
+++ Python/14LongestCommonPrefix.py
@@ -1,21 +1,16 @@
 '''
 Write a function to find the longest common prefix string amongst an array of 
 strings.
-
 If there is no common prefix, return an empty string "".
-
 Note:
-
 All given inputs are in lowercase letters a-z.
 '''
-
 
 def longestCommonPrefix(self, strs):
     """
     :type strs: List[str]
     :rtype: str
     """
-
     if not strs: return ""
     for i in range(len(strs[0])):
         for item in strs[1:]:
