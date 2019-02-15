b2ce70e8eaac0694f882a06d01148670cea3e5a1
niedexi <niedexi10@gmail.com>
2019-02-14 19:51:43 -0500

    added 26 remove duplicates from sorted array

---
+++ Python/2AddTwoNumbers.py
@@ -0,0 +1,21 @@
+'''
+You are given two non-empty linked lists representing two non-negative integers. 
+The digits are stored in reverse order and each of their nodes contain a single 
+digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the 
+number 0 itself.
+
+Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+'''
+
+def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
