953b5eedacac287131dc0f443a4e76819625fbaa
niedexi <niedexi10@gmail.com>
2019-02-14 19:53:33 -0500

    clean up 26.js

--- JavaScript/26.js
+++ JavaScript/26.js
@@ -1,15 +1,15 @@
-/**
- *26. Remove Duplicates from Sorted Array
+/*
+26. Remove Duplicates from Sorted Array
 
- *Given a sorted array nums, remove the duplicates in-place such that each element
- *appear only once and return the new length.
+Given a sorted array nums, remove the duplicates in-place such that each element
+appear only once and return the new length.
 
- *Do not allocate extra space for another array, you must do this by modifying the
- *input array in-place with O(1) extra memory.
+Do not allocate extra space for another array, you must do this by modifying the
+input array in-place with O(1) extra memory.
 
- * @param {number[]} nums
- * @return {number}
- */
+@param {number[]} nums
+@return {number}
+*/
 
 var removeDuplicates = function(nums) {
     if (nums.length == 0) return 0;
