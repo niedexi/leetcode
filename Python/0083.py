1381100a699aac04b3ecf262afef567fe24e35f4
niedexi <niedexi10@gmail.com>
2023-02-01 20:41:31 -0500

    update 0083.py

--- Python/0083.py
+++ Python/0083.py
@@ -11,6 +11,8 @@ Return the linked list sorted as well.
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        if not head or head.next: return head
         slow = head
         fast = head
 
@@ -21,4 +23,37 @@ class Solution:
             fast = fast.next
 
         slow.next = None
+        return head
+
+
+        # sample answer
+        if not head or not head.next:
+            return head
+        prev=head
+        ptr=head.next
+        while(ptr):
+            while(ptr and ptr.val==prev.val):
+                ptr=ptr.next
+            prev.next=ptr
+            prev=ptr
+            if ptr:
+                ptr=ptr.next
+        return head
+
+
+        # sample answer
+        cur = head
+        while cur and cur.next:
+            if cur.val == cur.next.val:
+                cur.next = cur.next.next
+            else:
+                cur = cur.next
+        return head
+
+        #sample
+        cur=head
+        while cur:
+            while cur.next and cur.next.val == cur.val:
+                cur.next = cur.next.next
+            cur=cur.next
         return head
